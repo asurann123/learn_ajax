73840d5324d65fa6ca766c0ae3b8e25b56f5ed40
SotaNaoi <>
2020-09-14 13:23:13 +0900

    コメントしたり

--- get_news.js
+++ get_news.js
@@ -92,10 +92,13 @@ function callback(request,mode){
       if (img_link !== null) {
         var img_link_dict = {title:title_ele.textContent, imglink:img_link.getAttribute('href'), pagelink:url.textContent, summarycontent:summary.textContent};
         img_links_array.push(img_link_dict);
+        //1枚ずつカードを作成する
         html +='<div class="card"><img class="bd-placeholder-img card-img-top" src="" alt=""><div class="card-body"><h5 class="card-title"><b>' + img_link_dict['title'] + '</b></h5><p class="card-text">' + img_link_dict['summarycontent'] + '</p><a href="' + img_link_dict['pagelink'] + '" class="btn btn-outline-info" target=”_blank”>詳しく</a></div></div><hr>'
       }
 
     }
+
+    //トップのカルーセルを生成
     top_imgs_html = '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><a href="' + img_links_array[0]['pagelink'] + '"><img class="d-block w-100" style="height: 15rem;" src="' + img_links_array[0]['imglink'] + '" alt="First slide"></a></div><div class="carousel-item"><a href="' + img_links_array[1]['pagelink'] + '"><img class="d-block w-100" style="height: 15rem;" src="' + img_links_array[1]['imglink'] + '" alt="Second slide"></div><div class="carousel-item"><a href="' + img_links_array[2]['pagelink'] + '"><img class="d-block w-100" style="height: 15rem;" src="' + img_links_array[2]['imglink'] + '" alt="Third slide"></div>  </div><a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>';
     top_imgs.innerHTML = top_imgs_html;
     main_contents.innerHTML = html;
